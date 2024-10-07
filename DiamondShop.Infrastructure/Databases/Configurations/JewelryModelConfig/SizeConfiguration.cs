779e874c3c812e119f13163366b0f1bc071e93e5
Pham Quang Thai <binlambaobao142@gmail.com>
2024-10-07 15:15:46 +0700

    fix sizeConfiguration

--- DiamondShop.Infrastructure/Databases/Configurations/JewelryModelConfig/SizeConfiguration.cs
+++ DiamondShop.Infrastructure/Databases/Configurations/JewelryModelConfig/SizeConfiguration.cs
@@ -13,19 +13,9 @@ namespace DiamondShop.Infrastructure.Databases.Configurations.JewelryModelConfig
 {
     internal class SizeConfiguration : IEntityTypeConfiguration<Size>
     {
-        protected static List<Size> SIZES = new List<Size>
-        {
-            Size.Create(3,null, SizeId.Parse(1.ToString())),
-            Size.Create(4,null, SizeId.Parse(2.ToString())),
-            Size.Create(5,null, SizeId.Parse(3.ToString())),
-            Size.Create(6,null, SizeId.Parse(4.ToString())),
-            Size.Create(7,null, SizeId.Parse(5.ToString())),
-            Size.Create(8,null, SizeId.Parse(6.ToString())),
-            Size.Create(9,null, SizeId.Parse(7.ToString())),
-            Size.Create(10,null, SizeId.Parse(8.ToString())),
-            Size.Create(11,null, SizeId.Parse(9.ToString())),
-            Size.Create(12,null, SizeId.Parse(10.ToString()))
-        };
+        protected static List<Size> SIZES = 
+            Enumerable.Range(SizeRules.MinRingSize, SizeRules.MaxRingSize)
+            .Select(p => Size.Create(p, null, SizeId.Parse(p.ToString()))).ToList();
         public void Configure(EntityTypeBuilder<Size> builder)
         {
             builder.ToTable("Size");
@@ -35,7 +25,6 @@ namespace DiamondShop.Infrastructure.Databases.Configurations.JewelryModelConfig
                     dbValue => SizeId.Parse(dbValue));
             builder.HasKey(p => p.Id);
             builder.HasIndex(p => p.Id);
-            builder.HasData(Enumerable.Range(SizeRules.MinRingSize, SizeRules.MaxRingSize).Select(p => Size.Create(p)));
             builder.HasData(SIZES);
         }
     }
