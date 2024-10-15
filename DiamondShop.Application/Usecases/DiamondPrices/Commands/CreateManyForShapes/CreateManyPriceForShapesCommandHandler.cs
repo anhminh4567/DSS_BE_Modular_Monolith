901f2eea7997d570d5d0e87348a46125bacc7bcc
anhminh4567 <minhtran052003@gmail.com>
2024-10-15 21:05:25 +0700

    update mapping, update dbconnection to use from appsettings, print all environment on startup

---
+++ DiamondShop.Application/Usecases/DiamondPrices/Commands/CreateManyForShapes/CreateManyPriceForShapesCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using DiamondShop.Domain.Models.DiamondPrices;
+using FluentResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiamondShop.Application.Usecases.DiamondPrices.Commands.CreateManyForShapes
+{
+    public record CreateManyPriceForShapesCommand() : IRequest<Result<List<DiamondPrice>>>;
+    internal class CreateManyPriceForShapesCommandHandler
+    {
+    }
+}
