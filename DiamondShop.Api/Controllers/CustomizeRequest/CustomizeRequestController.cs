dda4c151d00b31437e86f2eea9de2574730112ac
Pham Quang Thai <binlambaobao142@gmail.com>
2024-11-02 19:02:47 +0700

    add customize request controller #2

--- DiamondShop.Api/Controllers/CustomizeRequest/CustomizeRequestController.cs
+++ DiamondShop.Api/Controllers/CustomizeRequest/CustomizeRequestController.cs
@@ -1,5 +1,12 @@
-﻿using MapsterMapper;
+﻿using DiamondShop.Application.Commons.Responses;
+using DiamondShop.Application.Dtos.Responses.CustomizeRequest;
+using DiamondShop.Application.Services.Interfaces;
+using DiamondShop.Application.Usecases.CustomizeRequests.Commands.SendRequest;
+using DiamondShop.Application.Usecases.CustomizeRequests.Queries.GetAll;
+using DiamondShop.Domain.Models.RoleAggregate;
+using MapsterMapper;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DiamondShop.Api.Controllers.CustomRequest
@@ -17,14 +24,34 @@ namespace DiamondShop.Api.Controllers.CustomRequest
             _mapper = mapper;
         }
         [HttpGet("Admin/All")]
-        public async Task<ActionResult> GetAll()
+        public async Task<ActionResult> GetAll([FromQuery] GetAllCustomizeRequestQuery getAllCustomizeRequestQuery)
         {
-            return Ok();
+            var result = await _sender.Send(getAllCustomizeRequestQuery);
+            if (result.IsSuccess)
+            {
+                var mappedResult = _mapper.Map<PagingResponseDto<CustomizeRequestDto>>(result.Value);
+                return Ok(mappedResult);
+            }
+            return MatchError(result.Errors, ModelState);
         }
         [HttpPost("Send")]
-        public async Task<ActionResult> SendRequest()
+        [Authorize(Roles = AccountRole.CustomerId)]
+        public async Task<ActionResult> SendRequest([FromBody] CustomizeModelRequest customizeModelRequest)
         {
-            return Ok();
+            var userId = User.FindFirst(IJwtTokenProvider.USER_ID_CLAIM_NAME);
+            if (userId != null)
+            {
+                var result = await _sender.Send(new SendCustomizeRequestCommand(userId.Value, customizeModelRequest));
+                if (result.IsSuccess)
+                {
+                    var mappedResult = _mapper.Map<CustomizeRequestDto>(result.Value);
+                    return Ok(mappedResult);
+                }
+                else
+                    return MatchError(result.Errors, ModelState);
+            }
+            else
+                return Unauthorized();
         }
     }
 }
---
+++ DiamondShop.Application/Dtos/Responses/CustomizeRequest/CustomizeRequestDto.cs
@@ -0,0 +1,31 @@
+﻿using DiamondShop.Application.Dtos.Responses.Accounts;
+using DiamondShop.Application.Dtos.Responses.JewelryModels;
+using DiamondShop.Domain.Models.AccountAggregate;
+using DiamondShop.Domain.Models.AccountAggregate.ValueObjects;
+using DiamondShop.Domain.Models.CustomizeRequests.Entities;
+using DiamondShop.Domain.Models.CustomizeRequests.Enums;
+using DiamondShop.Domain.Models.JewelryModels;
+using DiamondShop.Domain.Models.JewelryModels.Entities;
+using DiamondShop.Domain.Models.JewelryModels.ValueObjects;
+
+namespace DiamondShop.Application.Dtos.Responses.CustomizeRequest
+{
+    public class CustomizeRequestDto
+    {
+        public string AccountId { get; set; }
+        public AccountDto Account { get; set; }
+        public string JewelryModelId { get; set; }
+        public JewelryModelDto JewelryModel { get; set; }
+        public string SizeId { get; set; }
+        public SizeDto Size { get; set; }
+        public MetalId MetalId { get; set; }
+        public MetalDto Metal { get; set; }
+        public string? EngravedText { get; set; }
+        public string? EngravedFont { get; set; }
+        public string? Note { get; set; }
+        public CustomizeRequestStatus Status { get; set; }
+        public List<DiamondRequestDto> DiamondRequests { get; set; } = new();
+        public string? SideDiamondId { get; set; }
+        public SideDiamondOptDto? SideDiamond { get; set; }
+    }
+}
