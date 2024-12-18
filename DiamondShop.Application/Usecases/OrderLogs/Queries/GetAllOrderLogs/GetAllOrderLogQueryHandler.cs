8e42f0479cc4938ada63afbbc4e962e5401f1c71
anhminh4567 <minhtran052003@gmail.com>
2024-12-18 07:43:29 +0700

    hot fix order by asc

--- DiamondShop.Application/Usecases/OrderLogs/Queries/GetAllOrderLogs/GetAllOrderLogQueryHandler.cs
+++ DiamondShop.Application/Usecases/OrderLogs/Queries/GetAllOrderLogs/GetAllOrderLogQueryHandler.cs
@@ -71,7 +71,7 @@ namespace DiamondShop.Application.Usecases.OrderLogs.Queries.GetAllOrderLogs
                     }
                 }
             }
-            var mappedLogs = getParentLog.OrderByDescending(x => x.CreatedDate).Adapt<List<OrderLogDto>>(config); //_mapper.Map<List<OrderLogDto>>(logs);
+            var mappedLogs = getParentLog.OrderBy(x => x.CreatedDate).Adapt<List<OrderLogDto>>(config); //_mapper.Map<List<OrderLogDto>>(logs);
             //getMappedLog.PreviousLog = mappedLogs.FirstOrDefault(x => x.Id == getMappedLog.PreviousLogId);
             
 
